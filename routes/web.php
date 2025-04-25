547247641ded075981267ab13a7a3ad1c40f44e3
SadewaNickz <Bagussadewa444@gmail.com>
2025-04-25 09:47:11 +0700

    commit pertama

--- routes/web.php
+++ routes/web.php
@@ -1,33 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\ProductController;
 
-// Dashboard Route
-Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+Route::get('/', function () {
+    return view('welcome');
+});
 
-// Product Routes
-Route::get('/product', [ProductController::class, 'index'])->name('product');
-Route::get('/product/create', [ProductController::class, 'create'])->name('product.create');
-Route::post('/product', [ProductController::class, 'store'])->name('product.store');
-Route::get('/product/{id}/edit', [ProductController::class, 'edit'])->name('product.edit');
-Route::put('/product/{id}', [ProductController::class, 'update'])->name('product.update');
-Route::delete('/product/{id}', [ProductController::class, 'destroy'])->name('product.destroy');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-// Placeholder routes for other sections
-Route::get('/order-list', function () {
-    return view('order-list');
-})->name('order-list');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-Route::get('/customer', function () {
-    return view('customer');
-})->name('customer');
-
-Route::get('/operator', function () {
-    return view('operator');
-})->name('operator');
-
-Route::get('/history', function () {
-    return view('history');
-})->name('history');
+require __DIR__.'/auth.php';
